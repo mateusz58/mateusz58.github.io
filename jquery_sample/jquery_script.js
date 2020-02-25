e19ab5da4398164fdee18a0f4cced9f1adadeb61
mateusz58 <matp321@gmail.com>
2020-02-25 12:04:38 +0300

    Removed idea file

--- jquery_sample/jquery_script.js
+++ jquery_sample/jquery_script.js
@@ -1,5 +1,5 @@
-$( document ).ready(function() {
-    $("#button_1").click(function() {
-       $("#div_1").toggle();
-    });
+$(document).ready(function () {
+  $("#button_1").click(function () {
+    $("#div_1").toggle();
+  });
 });
