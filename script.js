e3676b4e17d5f3a541cf27e212d6ef133e5cb25d
mateusz58 <matp321@gmail.com>
2020-07-06 00:44:14 +0200

    Changing few values

--- script.js
+++ script.js
@@ -1,239 +1,156 @@
-$(document).ready(function() {
-    const apiRoot = 'localhost:8081/api/v1/';
-    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-    const $tasksContainer = $('[data-tasks-container]');
-    console.log('jquery is working');
+$(document).ready(function(){
 
-  var availableBoards = {};
-  var availableTasks = {};
+    $("#update").hide();
 
-  // init
+    assignDataToTable();
 
-  getAllTasks();
-    $('#task-result').hide();
-
-  function getAllAvailableBoards(callback, callbackArgs) {
-    var requestUrl = apiRoot + 'trello/boards';
-
-    $.ajax({
-      url: requestUrl,
-      method: 'GET',
-      contentType: 'application/json',
-      success: function(boards) { callback(callbackArgs, boards); }
-    });
-  }
-
-    function createElement(data) {
-        console.log('Element create');
-        const element = $(datatableRowTemplate).clone();
-
-        element.attr('data-task-id', data.id);
-        element.find('[data-task-name-section] [data-task-name-paragraph]').text(data.title);
-        element.find('[data-task-name-section] [data-task-name-input]').val(data.title);
-
-        element.find('[data-task-content-section] [data-task-content-paragraph]').text(data.content);
-        element.find('[data-task-content-section] [data-task-content-input]').val(data.content);
-
-        return element;
-    }
-
-    $('#task-form').submit(e => {
-        e.preventDefault();
-        const postData = {
-            name: $('#name').val(),
-            description: $('#description').val(),
-            id: $('#taskId').val()
-        };
-        const url = apiRoot + 'tasks';
-        console.log(postData, url);
-        $.post(url, postData, (response) => {
-            console.log(response);
-            $('#task-form').trigger('reset');
-            fetchTasks();
-        });
-    });
-
-    function getAllTasks() {
-        const requestUrl = apiRoot + 'tasks';
-        console.log('Requesting url ' + requestUrl);
+    $('table').on('click', 'button[id="delete"]', function(e){
+        var id = $(this).closest('tr').children('td:first').text();
 
         $.ajax({
-            url: requestUrl,
-            method: 'GET',
-            contentType: "application/json",
-            success: function(response) {
-                const tasks = JSON.parse(response);
-                let template = '';
-                console.log('Reponse call' + response);
-                tasks.forEach(task => {
-                    template += `
-                  <tr taskId="${task.id}">
-                  <td>${task.id}</td>
-                  <td>
-                  <a href="#" class="task-item">
-                    ${task.title} 
-                  </a>
-                  </td>
-                  <td>${task.content}</td>
-                  <td>
-                    <button class="task-delete btn btn-danger">
-                     Delete 
-                    </button>
-                  </td>
-                  </tr>
-                `
-                });
-                $('#tasks').html(template);
+            type:"DELETE",
+            url:"http://localhost:8081/api/v1/tasks" + id,
+            success: function(data){
+                assignDataToTable();
+            },
+            error: function(err) {
+                console.log(err);
+                alert(err);
             }
         });
-    }
 
-    function prepareBoardOrListSelectOptions(availableChoices) {
-        return availableChoices.map(function(choice) {
-            return $('<option>')
-                .addClass('crud-select__option')
-                .val(choice.id)
-                .text(choice.name || 'Unknown name');
-        });
-    }
-    function handleDatatableRender(taskData, boards) {
-        $tasksContainer.empty();
-        boards.forEach(board => {
-            availableBoards[board.id] = board;
-        });
+    })
 
-        taskData.forEach(function(task) {
-            var $datatableRowEl = createElement(task);
-            var $availableBoardsOptionElements = prepareBoardOrListSelectOptions(boards);
+    $('table').on('click', 'button[id="edit"]', function(e){
+        var id = $(this).closest('tr').children('td:first').text();
+        var name = $(this).closest('tr').children('td:nth-child(2)').text();
+        var age = $(this).closest('tr').children('td:nth-child(3)').text();
+        var book = $(this).closest('tr').children('td:nth-child(4)').text();
 
-            $datatableRowEl.find('[data-board-name-select]')
-                .append($availableBoardsOptionElements);
+        $("#name").val(name);
+        $("#age").val(age);
+        $("#book").val(book);
 
-            $datatableRowEl
-                .appendTo($tasksContainer);
-        });
-    }
+        $("#update").show();
+        $("#save").hide();
 
-    function handleTaskUpdateRequest() {
-        var parentEl = $(this).parents('[data-task-id]');
-        var taskId = parentEl.attr('data-task-id');
-        var taskTitle = parentEl.find('[data-task-name-input]').val();
-        var taskContent = parentEl.find('[data-task-content-input]').val();
-        var requestUrl = apiRoot + 'tasks';
+        $("#update").click(function() {
 
-        $.ajax({
-            url: requestUrl,
-            method: "PUT",
-            processData: false,
-            contentType: "application/json; charset=utf-8",
-            dataType: 'json',
-            data: JSON.stringify({
-                id: taskId,
-                title: taskTitle,
-                content: taskContent
-            }),
-            success: function(data) {
-                parentEl.attr('data-task-id', data.id).toggleClass('datatable__row--editing');
-                parentEl.find('[data-task-name-paragraph]').text(taskTitle);
-                parentEl.find('[data-task-content-paragraph]').text(taskContent);
-            }
-        });
-    }
+            var ageNum = parseInt($("#age").val());
 
-    function handleTaskDeleteRequest() {
-        var parentEl = $(this).parents('[data-task-id]');
-        var taskId = parentEl.attr('data-task-id');
-        var requestUrl = apiRoot + 'tasks';
+            var jsonVar = {
+                name: $("#name").val(),
+                age: ageNum,
+                book: $("#book").val()
+            };
 
-        $.ajax({
-            url: requestUrl + '/' + taskId,
-            method: 'DELETE',
-            success: function() {
-                parentEl.slideUp(400, function() { parentEl.remove(); });
-            }
-        })
-    }
-
-    function handleTaskSubmitRequest(event) {
-        event.preventDefault();
-
-        var taskTitle = $(this).find('[name="title"]').val();
-        var taskContent = $(this).find('[name="content"]').val();
-
-        var requestUrl = apiRoot + 'tasks';
-
-        $.ajax({
-            url: requestUrl,
-            method: 'POST',
-            processData: false,
-            contentType: "application/json; charset=utf-8",
-            dataType: 'json',
-            data: JSON.stringify({
-                title: taskTitle,
-                content: taskContent
-            }),
-            complete: function(data) {
-                if(data.status === 200) {
-                    getAllTasks();
+            $.ajax({
+                type:"PUT",
+                data: JSON.stringify(jsonVar),
+                contentType: "application/json",
+                url:"http://localhost:8080/api/users/" + id,
+                success: function(data){
+                    $("#update").hide();
+                    $("#save").show();
+                    $("#name").val("");
+                    $("#age").val("");
+                    $("#book").val("");
+                    assignDataToTable();
+                },
+                error: function(err) {
+                    console.log(err);
+                    alert(err);
                 }
+
+            });
+
+        });
+
+    })
+
+    var age = $("#age");
+
+    age.keypress(function(key){
+        if(key.charCode > 48 && key.charCode < 57){
+            if(age.val().length < 3){
+                return true;
+            }else{
+                alertUsing("3 Haneyi Aştınız.", false);
+                return false;
+            }
+        }else{
+            alertUsing("Sayı Giriniz.", false);
+            return false;
+        }
+    });
+
+    $("#save").click(function() {
+
+        var jsonVar = {
+            title: $("#title").val(),
+            content: $("#content").val(),
+        };
+
+        $.ajax({
+            type:"POST",
+            url:"http://localhost:8081/api/v1/tasks",
+            data: JSON.stringify(jsonVar),
+            contentType: "application/json",
+            success: function(data){
+                assignDataToTable();
+            },
+            error: function(err) {
+                console.log(err);
+                alert(err);
             }
         });
+
+    });
+
+    function assignDataToTable() {
+        $("tbody").empty();
+        $.ajax({
+            type:"GET",
+            contentType: "application/json",
+            url:"http://localhost:8081/api/v1/tasks",
+            success: function(data) {
+                var tasks = JSON.parse(JSON.stringify(data));
+                for (var i in tasks) {
+                    $("tbody").
+                    append("<tr> \
+                            <td>" +  tasks[i].id + "</td> \
+                            <td>" +  tasks[i].title + "</td> \
+                            <td>" +  tasks[i].content + "</td> \
+                            <td> \ <button id='delete' class='btn btn-danger'>Sil</button> \
+                           <button id='edit' class='btn btn-warning'>Düzenle</button> \ </td> \
+                        </tr>");
+                }
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+
     }
 
-    function toggleEditingState() {
-        var parentEl = $(this).parents('[data-task-id]');
-        parentEl.toggleClass('datatable__row--editing');
+    function alertUsing(text, flag) {
 
-        var taskTitle = parentEl.find('[data-task-name-paragraph]').text();
-        var taskContent = parentEl.find('[data-task-content-paragraph]').text();
+        var alert = $(".alert");
 
-        parentEl.find('[data-task-name-input]').val(taskTitle);
-        parentEl.find('[data-task-content-input]').val(taskContent);
-    }
+        if(flag){
+            alert.removeClass("alert-danger").addClass("alert-success");
+        }else{
+            alert.removeClass("alert-success").addClass("alert-danger");
 
-    function handleBoardNameSelect(event) {
-        var $changedSelectEl = $(event.target);
-        var selectedBoardId = $changedSelectEl.val();
-        var $listNameSelectEl = $changedSelectEl.siblings('[data-list-name-select]');
-        var preparedListOptions = prepareBoardOrListSelectOptions(availableBoards[selectedBoardId].lists);
-
-        $listNameSelectEl.empty().append(preparedListOptions);
-    }
-
-    function handleCardCreationRequest(event) {
-        var requestUrl = trelloApiRoot + 'cards';
-        var $relatedTaskRow = $(event.target).parents('[data-task-id]');
-        var relatedTaskId = $relatedTaskRow.attr('data-task-id');
-        var relatedTask = availableTasks[relatedTaskId];
-        var selectedListId = $relatedTaskRow.find('[data-list-name-select]').val();
-
-        if (!selectedListId) {
-            alert('You have to select a board and a list first!');
-            return;
         }
 
-        $.ajax({
-            url: requestUrl,
-            method: 'POST',
-            processData: false,
-            contentType: "application/json; charset=utf-8",
-            dataType: 'json',
-            data: JSON.stringify({
-                name: relatedTask.title,
-                description: relatedTask.content,
-                listId: selectedListId
-            }),
-            success: function(data) {
-                console.log('Card created - ' + data.shortUrl);
-            }
-        });
+        alert.fadeIn(400);
+        alert.css("display", "block");
+        alert.text(text);
+        setTimeout(function() {
+            alert.fadeOut();
+        }, 2000);
+
     }
 
-    $('[data-task-add-form]').on('submit', handleTaskSubmitRequest);
-    $tasksContainer.on('change','[data-board-name-select]', handleBoardNameSelect);
-    $tasksContainer.on('click','[data-trello-card-creation-trigger]', handleCardCreationRequest);
-    $tasksContainer.on('click','[data-task-edit-button]', toggleEditingState);
-    $tasksContainer.on('click','[data-task-edit-abort-button]', toggleEditingState);
-    $tasksContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
-    $tasksContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
 });
